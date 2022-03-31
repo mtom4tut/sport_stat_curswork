992efd1861411890337443f0f51bf516d728d9a6
m.tutukov <m.tutukov@kokoc.tech>
2022-03-31 22:41:54 +0300

    поиск

--- src/widgets/TableList/TableList.tsx
+++ src/widgets/TableList/TableList.tsx
@@ -5,10 +5,12 @@ import { IDataTable } from '~features/addTableForm/model/types';
 
 // Components
 import { TableItem } from '~entities/TableItem';
+import { Input } from 'antd';
 
 // Styles
 import cl from 'classnames';
 import styles from './TableList.module.scss';
+import { useState } from 'react';
 
 interface TableListProps {
   className?: string;
@@ -16,6 +18,8 @@ interface TableListProps {
 }
 
 export const TableList: FC<TableListProps> = ({ className, data }) => {
+  const [filterData, setFilterData] = useState<IDataTable[]>(data);
+
   function getName(item: IDataTable): string {
     const elem = item.valueRanges.find(item => item.range === "'Спортсмен'!A1:Z1000");
     const elemItem = elem?.values.find((_, index) => index === 1);
@@ -27,11 +31,26 @@ export const TableList: FC<TableListProps> = ({ className, data }) => {
     return 'Имя не распознано';
   }
 
+  function filteredData(val: string) {
+    if (val) {
+      val = val.toLowerCase();
+      const filter = data.filter(
+        item => item.spreadsheetId.toLowerCase().includes(val) || getName(item).toLowerCase().includes(val)
+      );
+      setFilterData(filter);
+    } else {
+      setFilterData(data);
+    }
+  }
+
   return (
-    <ul className={cl(className, styles['table-list'])}>
-      {data.map(item => (
-        <TableItem key={item.spreadsheetId} id={item.spreadsheetId} name={getName(item)} />
-      ))}
-    </ul>
+    <div>
+      <Input allowClear placeholder='Начните вводить id таблицы или ФИО спортсмена' onChange={e => filteredData(e.target.value)} />
+      <ul className={cl(className, styles['table-list'])}>
+        {filterData.map(item => (
+          <TableItem key={item.spreadsheetId} id={item.spreadsheetId} name={getName(item)} />
+        ))}
+      </ul>
+    </div>
   );
 };
